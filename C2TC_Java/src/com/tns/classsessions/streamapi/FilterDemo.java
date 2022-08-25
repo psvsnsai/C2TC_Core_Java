245e5bd783b31ea639bb811867ad57ccaa20b162
psvsn <psvsn@DESKTOP-HG8PPVS>
2022-08-25 14:40:59 +0530

    practice

---
+++ C2TC_Java/src/com/tns/classsessions/streamapi/FilterDemo.java
@@ -0,0 +1,22 @@
+package com.tns.classsessions.streamapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterDemo {
+
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("Ravi", "Teja", "Pavan", "Kalyan", "Joffery");
+		
+		List<String> longnames = new ArrayList<String>();
+		
+		longnames=names.stream().filter(str-> str.length()>6 && str.length()<8).collect(Collectors.toList());
+		System.out.println(longnames);
+		
+//		names.stream().filter(str-> str.length()>6 && str.length()<8).forEach(str->System.out.println(str));
+
+	}
+
+}
