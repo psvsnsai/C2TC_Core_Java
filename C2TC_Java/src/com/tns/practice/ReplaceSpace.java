95a81694f3aea44814d013a458a6e71374f0fb50
psvsn <psvsn@DESKTOP-HG8PPVS>
2022-08-25 14:15:52 +0530

    practice

---
+++ C2TC_Java/src/com/tns/practice/ReplaceSpace.java
@@ -0,0 +1,15 @@
+package com.tns.practice;
+
+public class ReplaceSpace    
+{    
+    public static void main(String[] args) {    
+        String string = "Once upon a time";    
+        char ch = '-';    
+            
+        //Replace space with specific character ch    
+        string = string.replace(' ', ch);    
+            
+        System.out.println("String after replacing spaces with given character: ");    
+        System.out.println(string);    
+    }    
+}      
